64f6497fac5f8fa667e7226e2551b6dee8dd6bde
lhgiang040504 <lhgiang040504@gmail.com>
2023-06-22 10:49:41 +0700

    the lowest common ancestor solved but stil gets a little wrong

---
+++ binary_tree/LCA.cpp
@@ -0,0 +1,89 @@
+// lowest common ancestor
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+  
+    Node *lca(Node *root, int v1,int v2) {
+        int greater = (v1 + v2) / 2.0 + abs(v1 - v2) / 2.0;
+        int smaller = (v1 + v2) / 2.0 - abs(v1 - v2) / 2.0;
+        
+        int ancestor = root->data;
+
+        if (ancestor > greater) 
+            root = root->left;
+        else if (ancestor <= smaller)
+            root = root->right;
+        else
+            return root;
+        
+        return lca(root, v1, v2);
+
+    }
+
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  	
+  	int v1, v2;
+  	std::cin >> v1 >> v2;
+  
+    Node *ans = myTree.lca(root, v1, v2);
+    
+  	std::cout << ans->data;
+
+    return 0;
+}
